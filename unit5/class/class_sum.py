e1eaed8e53077bdc608b04ce26f319c314ba562b
haribhakta <meharibhakta@gmail.com>
2024-07-23 14:57:17 +0545

    python commit of Opp chapter

--- unit5/class/class_sum.py
+++ unit5/class/class_sum.py
@@ -7,12 +7,15 @@ class Addition:
         self.a = int(input("Enter a number = "))
         self.b = int(input("Enter another number = "))
 
+
     def sum(self):
         self.c = self.a + self.b
         return self.c
-
-
 add = Addition()
 add.putvalue()
 print("Sum = ", add.sum())
+# WAP in python to enter length and breadth of a rectangle
+# and find its area by using class and object.
 
+# WAP in python to create a class that ask data of two car
+# (name,model and price)  then compare them with price.
